08a8c8f0d78f014f0ea4a617ae4dc778590d360d
Dylan Williamson <dylan@Dylans-MacBook-Pro.local>
2017-10-21 14:18:20 -0400

    first commit

--- PthreadProject/main.cpp
+++ PthreadProject/main.cpp
@@ -2,14 +2,152 @@
 //  main.cpp
 //  PthreadProject
 //
-//  Created by Dylan Williamson on 10/21/17.
+//  Created by Dylan Williamson on 10/20/17.
 //  Copyright © 2017 Dylan Williamson. All rights reserved.
 //
 
 #include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+using namespace std;
+
+int chances = 3;
+
+/* functions used by threads to start execution */
+void *sifter(void *param);
+void *fence(void *param);
+void *hill(void *param);
+void *pinnacle(void *param);
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    string message0 = "*first**second**third";
+    string message1 = "*first";
+    string message2 = "**second";
+    string message3 = "***third";
+    int result;
+    
+    
+    
+    
+    /* Define thread reference variables for each thread being created */
+    pthread_t sifterThread;
+    pthread_t fenceThread;
+    pthread_t hillThread;
+    pthread_t pinnacleThread;
+    
+    /* Create Thread to execute corresponding method */
+    
+    if(pthread_create(&sifterThread, NULL, sifter, &message0)) {
+        cout << "Error creating thread" << endl;
+        return 1;
+    }
+    
+    pthread_create(&fenceThread, NULL, fence, &message1);
+    pthread_create(&hillThread, NULL, hill, &message2);
+    pthread_create(&pinnacleThread, NULL, pinnacle, &message3);
+    
+    
+    
+    /*wait for thread to finish */
+    if(pthread_join(sifterThread, NULL)) {
+        cout << "Error joining thread" << endl;
+        
+        return 1;
+    }
+    pthread_join(fenceThread, NULL);
+    pthread_join(hillThread, NULL);
+    pthread_join(pinnacleThread, NULL);
+    
+    
+    cout << "Enter ciphered message: ";
+    cin >> result;
+    
+    
+    cout << "You entered: " << result << endl;
+    
+    
+    
+    
+    
     return 0;
 }
+
+/* Test sifter thread method */
+void *sifter(void *message_void_ptr) {
+    string *message0_ptr = (string *)message_void_ptr;
+    
+    cout << "Sifter Thread message: " << *message0_ptr << endl;
+    cout << "Sifter Thread done! Success!" << endl;
+    
+    
+    return NULL;
+}
+
+/* Test fence thread method */
+void *fence(void *message_void_ptr) {
+    string *message1_ptr = (string *)message_void_ptr;
+    
+    cout << "Fence Thread message: " << *message1_ptr << endl;
+    cout << "Fence Thread done! Success!" << endl;
+    
+    
+    return NULL;
+}
+
+/* Test Hill Thread method */
+void *hill(void *message_void_ptr) {
+    string *message2_ptr = (string *)message_void_ptr;
+    
+    cout << "Hill Thread message: " << *message2_ptr << endl;
+    cout << "Hill Thread done! Success!" << endl;
+    
+    
+    return NULL;
+}
+
+/* Test Pinnacle Thread method */
+void *pinnacle(void *message_void_ptr) {
+    string *message3_ptr = (string *)message_void_ptr;
+    
+    cout << "Pinnacle Thread message: " << *message3_ptr << endl;
+    cout << "Pinnalce Thread done! Success!" << endl;
+    
+    
+    return NULL;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
