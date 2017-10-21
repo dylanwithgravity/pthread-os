6ae4f2a62867b84c28e3cc4cc051f89e388e8964
Dylan Williamson <dylan@Dylans-MacBook-Pro.local>
2017-10-21 14:15:04 -0400

    Initial Commit

---
+++ PthreadProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PthreadProject
+//
+//  Created by Dylan Williamson on 10/21/17.
+//  Copyright © 2017 Dylan Williamson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
